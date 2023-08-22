53906c38f78427ab04d149ad7a47c36f6c21f725
me996 <me996@gmail.com>
2023-08-22 04:01:30 -0700

    putchar

--- _printf.c
+++ _printf.c
@@ -1,66 +1,55 @@
 #include "main.h"
-
-void print_buffer(char buffer[], int *buff_ind);
-
-/**
- * _printf - Printf function
- * @format: A character string.
- * Return: Number of characters printed.
+/*
+ * _printf - printf function
+ * @format: is a character string
+ * return: the number of characters printed
  */
 int _printf(const char *format, ...)
 {
-int i, printed = 0, printed_chars = 0;
-int flags, width, precision, size, buff_ind = 0;
-va_list list;
-char buffer[BUFF_SIZE];
-
-if (format == NULL)
-return (-1);
-
-va_start(list, format);
-
-for (i = 0; format && format[i] != '\0'; i++)
+va_list args;
+va_start(args, format);
+int printed_chars = 0;
+for (int i = 0; format[i] != '\0'; i++)
 {
-if (format[i] != '%')
+if (format[i] == '%')
 {
-buffer[buff_ind++] = format[i];
-if (buff_ind == BUFF_SIZE)
-print_buffer(buffer, &buff_ind);
-/* write(1, &format[i], 1);*/
+i++;
+switch (format[i])
+{
+case 'c':
+_putchar(va_arg(args, int));
 printed_chars++;
+break;
+case 's':
+const char *str = va_arg(args, const char *);
+while (*str)
+{
+_putchar(*str);
+str++;
+printed_chars++;
+}
+break;
+case '%':
+_putchar('%');
+printed_chars++;
+break;
+default:
+break;
+}
 }
 else
 {
-print_buffer(buffer, &buff_ind);
-flags = get_flags(format, &i);
-width = get_width(format, &i, list);
-precision = get_precision(format, &i, list);
-size = get_size(format, &i);
-++i;
-printed = handle_print(format, &i, list, buffer,
-flags, width, precision, size);
-if (printed == -1)
-return (-1);
-printed_chars += printed;
+_putchar(format[i]);
+printed_chars++;
 }
 }
-
-print_buffer(buffer, &buff_ind);
-
-va_end(list);
-
+va_end(args);
 return (printed_chars);
 }
-
-/**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index to add next char, represents the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
+int main(void)
 {
-if (*buff_ind > 0)
-write(1, &buffer[0], *buff_ind);
+_printf("Hello, %s! The character is %c.\n", "World", 'A');
 
-*buff_ind = 0;
+return (0);
 }
+
