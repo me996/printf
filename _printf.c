03ce138b4d8b02e29fa30b1185f9ef2e3066baea
me996 <me996@gmail.com>
2023-08-23 08:42:48 -0700

    task0

--- _printf.c
+++ _printf.c
@@ -1,20 +1,27 @@
 #include "main.h"
 /*
- *printf - Printf function
+ *_printf - Printf function
  * @format: format.
+ * handle_string : A function that takes argument
+ * _putchar : print a single character
  * Return: Printed chars.
  */
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
+
 int handle_string(char *str)
 {
 int i = 0;
-if (srt == NULL)
+if (str == NULL)
 {
-handle_string ("(NULL)");
-return;
+handle_string("(NULL)");
+return (i);
 }
-while (str[i]);
+while (str[i])
 {
-_putchar(str[i]);
+putchar(str[i]);
 i++;
 }
 return (i);
@@ -24,38 +31,42 @@ int _printf(const char *format, ...)
 int count = 0;
 va_list args;
 va_start(args, format);
-if (!format || !format[0]);
+if (!format || !format[0])
 return (-1);
 while (*format)
 {
 if (*format == '%')
-format++
+{
+format++;
 if (*format == 'c')
 {
 char c = va_arg(args, int);
 _putchar (c);
-count++
+count++;
 }
 else if (*format == 's')
 {
 char *str = va_arg(args, char *);
 count += handle_string(str);
 }
-else if (*format == '%');
+else if (*format == '%')
 {
-_putchar(%);
-count++
+_putchar('%');
+count++;
 }
 else
 {
 _putchar (*format);
-count;
+count++;
 }
+format++;
 }
-else 
+else
 {
 _putchar(*format);
 format++;
+count++;
 }
 }
+return (count);
 }
